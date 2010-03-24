98918595dc4468c64556045c05b9a4552ecb9f25
Frank Villasenor <Frank@TheOpenSourceU.com>
2010-03-23 22:20:57 -0500

    Unit test for DebugPrinter.

--- tests/com/theOpenSourceU/webserver/debugutil/DebugPrinterTests.java
+++ tests/com/theOpenSourceU/webserver/debugutil/DebugPrinterTests.java
@@ -1,20 +1,57 @@
 package com.theOpenSourceU.webserver.debugutil;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.Assert;
 
 public class DebugPrinterTests {
 	
+	ByteArrayOutputStream _out = null;
+	ByteArrayOutputStream _err = null;
+	DebugPrintable _p = null;
 	
+	@Before 
+	public void setUp() {
+		_out = new ByteArrayOutputStream();
+		_err = new ByteArrayOutputStream();
+		
+		System.setOut(new PrintStream(_out));
+		System.setErr(new PrintStream(_err));
+		_p = new DebugPrinter();
+	}
+	
+	@After 
+	public void tearDown() {
+		_out = null;
+		_err = null;
+		_p = null;
+	}
 	
 	@Test
-	public void fail() {
-		Assert.fail();
+	public void printMsg() {
+		_p.printMessage("test");
+		
+		final String output = _out.toString().trim();
+		final String errOutput = _err.toString().trim();
+		
+		Assert.assertTrue( output.contains("test") );
+		Assert.assertEquals("", errOutput);
 	}
 	
-	@Test 
-	public void success() {
-		Assert.assertEquals(1,1);
+	@Test
+	public void printErr() {
+		_p.printError("test-error");
+		
+		final String output = _out.toString();
+		final String errOutput = _err.toString();
+		
+		Assert.assertEquals("", output);
+		Assert.assertTrue( errOutput.contains("test-error") );
 	}
+	
 }
