f08a7572130e76f108dbc3abab5fd97b554f4ab0
Frank Villasenor <Frank@TheOpenSourceU.com>
2010-03-26 14:44:53 -0500

    Unit test for http's package StringUtils.

---
+++ tests/com/theOpenSourceU/webserver/http/StringUtilsTests.java
@@ -0,0 +1,19 @@
+package com.theOpenSourceU.webserver.http;
+
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class StringUtilsTests {
+
+	@Test
+	public void testRepeat() {
+		final String _dash = "-----";
+		final String _equals = "=====";
+		
+		Assert.assertEquals(_dash, StringUtils.repeat('-', 5));
+		Assert.assertEquals(_equals, StringUtils.repeat('=', 5));
+	}
+	
+}
